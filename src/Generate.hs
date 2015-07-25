733081c07cef8b9e4c3ed68d228194dff867eac5
S. M. Hickman <sean@grump.ly>
2015-07-25 00:34:16 -0500

    Working instruction derivation, co-instruction derivation, pairing derivation, and 80% complete interpreter derivation.

--- src/Generate.hs
+++ src/Generate.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Generate where
 
+import Control.Arrow
 import Control.Monad
 import Data.Char
-
+import Data.Maybe
 import Language.Haskell.TH
 
 type Params = [Name]
@@ -99,7 +102,7 @@ makeCo :: Name -> Q [Dec]
 makeCo instrName@(coName -> coInstrName) = do
   TyConI d <- reify instrName
   (_,params,_,terms0) <- typeInfo (return d)
-  let tvs = foldr ((:) . PlainTV) [] params
+  let tvs = map PlainTV params
   constructed <-
     if length terms0 > 1
     then makeClosed terms0
@@ -153,29 +156,185 @@ pairs co_instr instr = do
 
 pairs_ti :: TI -> TI -> Q [Dec]
 pairs_ti co_ti ti = do
-  let (nm,_,_,instr_terms) = ti
-      (co_nm,_,_,_) = co_ti
+  let (nm,params,_,instr_terms) = ti
+      (co_nm,co_params,_,_) = co_ti
       isClosed = length instr_terms > 1
   pairings <- if isClosed then createClosedPairing else createOpenPairing
-  let inst_head = ConT (mkName "Pairing") `AppT` ConT co_nm `AppT` ConT nm
+  when (length co_params /= length params) $
+    error $ "Variable parameter count must match between Instruction and Interpreter to automatically pair.\nParameters: (instruction,interpreter)\n\t"
+            ++ show (params,co_params)
+  let co_con = if length co_params <= 1
+               then ConT co_nm
+               else foldl (\st a -> AppT st (VarT a)) (ConT co_nm) $ init co_params
+      con = if length params <= 1
+            then ConT nm
+            else foldl (\st a -> AppT st (VarT a)) (ConT nm) $ init co_params
+      inst_head = ConT (mkName "Pairing") `AppT` co_con `AppT` con
   return [ InstanceD [] inst_head pairings ]
   where
-    clear = putStrLn ""
     createOpenPairing = do
+      let (co_nm,co_params,_,_) = co_ti
+      let (nm,params,_,_) = ti
+      return
+       [ FunD (mkName "pair")
+           [ Clause
+               [ VarP (mkName "p")
+               , ConP co_nm [VarP $ head co_params]
+               , ConP nm (map VarP params)
+               ]
+               (NormalB (VarE (mkName "p")
+                         `AppE` foldl (\st a -> AppE st (VarE a))
+                                      (VarE (head co_params))
+                                      (init params)
+                         `AppE` (VarE $ last params)
+                        )
+               )
+               []
+           ]
+       ]
+    createClosedPairing = do
       let (co_nm,co_params,co_cons,co_terms) = co_ti
-      runIO $ do
-        print co_nm >> clear
-        mapM_ print co_params >> clear
-        mapM_ print co_cons >> clear
-        mapM_ print co_terms >> clear
-      let (nm,params,cons,terms) = ti
-      runIO $ do
-        print nm >> clear
-        mapM_ print params >> clear
-        mapM_ print cons >> clear
-        mapM_ print terms >> clear
-      return []
-    createClosedPairing = undefined
+          (nm,params,cons,terms) = ti
+          (co_con,co_recs) = head co_terms
+      when (length co_cons > 1) $ error $
+        "Closed pairing does not yet support interpreter variants.\nIf you have a use for this, please suggest it on:\n\tgithub.com/grumply/mop/issues"
+      when (snd (head co_cons) /= length cons) $ error $
+        "Closed interpreter expected to have " ++
+           show (length cons) ++ " records, but found " ++
+           show (snd (head co_cons))
+      let recPs = map (fromJust . fst) co_recs
+      forM (zip (zip [0 :: Int ..] $ map fst cons) terms) $ \((tc,tcon),(_,ts)) -> do
+        ts' <- map fst <$> nameTerms ts
+        let ps = reverse $ snd $
+                   foldl
+                     (\(i,st) a -> if i == tc
+                                   then (i+1,VarP a:st)
+                                   else (i+1,WildP:st)
+                     )
+                     (0,[])
+                     recPs
+        return $
+         FunD (mkName "pair")
+         [ Clause
+             [ VarP (mkName "p")
+             , ConP co_nm ps
+             , ConP tcon $ map VarP ts'
+             ]
+             (NormalB $
+               (VarE $ mkName "p")
+               `AppE`
+               foldl AppE
+                     (VarE $ recPs !! tc)
+                     (init $ map VarE ts')
+               `AppE`
+               VarE (last ts')
+
+             )
+             []
+         ]
+
+makeInterpreter :: Name -> Q [Dec]
+makeInterpreter co_instr_nm = do
+  TyConI cod <- reify co_instr_nm
+  ti@((co_nm,_,co_cons,co_terms) :: TI) <- typeInfo (return cod)
+  if snd (head co_cons) > 1
+  then makeClosedInterpreter ti
+  else makeOpenInterpreter ti
+  where
+   makeClosedInterpreter :: TI -> Q [Dec]
+   makeClosedInterpreter ti@(co_nm,_,co_cons,co_terms) = do
+     let defRet = ConE (mkName "Identity") `AppE` VarE (mkName "id")
+         ts = map (VarE . fromJust . fst) . snd $ head co_terms
+         coalgebraBuilder = foldl
+           (flip UInfixE (VarE $ mkName "<*>"))
+           (UInfixE (ConE co_nm) (VarE $ mkName "<$>") (head ts))
+           (tail ts)
+     coalgebra <- makeOpenInterpreter ti
+     return
+       [ FunD (mkName $ "mk" ++ nameBase co_nm)
+          [Clause
+            []
+            (NormalB $
+                     VarE (mkName "coiterT")
+              `AppE` VarE (mkName "next")
+              `AppE` VarE (mkName "start")
+            )
+            [ FunD (mkName "start") [Clause [] (NormalB defRet) []]
+            , FunD (mkName "next") [Clause [] (NormalB coalgebraBuilder) coalgebra]
+            ]
+          ]
+       ]
+   makeOpenInterpreter :: TI -> Q [Dec]
+   makeOpenInterpreter (co_nm,_,co_cons,co_terms) = do
+     wa <- newName "wa"
+     runIO $ do
+       print co_cons
+       print co_terms
+     let defRet = ConE (mkName "Identity") `AppE` VarE (mkName "id")
+         ts = map (VarE . fromJust . fst) . snd $ head co_terms
+         coalgebraBuilder = foldl
+           (flip UInfixE (VarE $ mkName "<*>"))
+           (UInfixE (ConE co_nm) (VarE $ mkName "<$>") (head ts))
+           (tail ts)
+         ct = snd $ last $ snd $ last co_terms
+     runIO (print ct)
+     if length (snd $ head co_terms) > 1
+     then return $ flip map (snd $ head co_terms) $ \(Just nm,ty) ->
+              FunD
+                 nm
+                 [ Clause
+                 ([VarP wa] ++ (convertToPat $ last $ collectVars ct))
+                 (NormalB $ makeEmptyFunction wa ct)
+                 []
+             ]
+
+     else return $
+         [ FunD
+             (mkName $ "mk" ++ nameBase co_nm)
+             [ Clause
+                 []
+                 (NormalB $
+                          VarE (mkName "coiterT")
+                   `AppE` VarE (mkName "next")
+                   `AppE` VarE (mkName "start")
+                 )
+                 [ FunD (mkName "start") [Clause [] (NormalB defRet) []]
+                 , FunD (mkName "next") [Clause [] (NormalB coalgebraBuilder) []]
+                 ]
+             ]
+         , FunD
+             (lowerName co_nm)
+             [ Clause
+                 ([VarP wa] ++ (convertToPat $ last $ collectVars ct))
+                 (NormalB $ makeEmptyFunction wa ct)
+                 []
+             ]
+         ]
+
+convertToPat :: Type -> [Pat]
+convertToPat (AppT a b) = convertToPat a ++ convertToPat b
+convertToPat (VarT v) = [VarP v]
+convertToPat (ConT v) = [ConP v []]
+convertToPat _ = []
+
+makeEmptyFunction :: Name -> Type -> Exp
+makeEmptyFunction em (pred . countVars -> n) = TupE $ (replicate n (VarE (mkName "undefined"))) ++ [VarE em]
+
+collectVars :: Type -> [Type]
+collectVars = init . go
+  where
+    go (AppT t0 t1) = go t0 ++ go t1
+    go vt@(VarT _) = [vt]
+    go _ = []
+
+countVars (AppT (TupE _) x) = countVars x
+countVars (AppT ArrowT (VarT _)) = 0
+countVars (AppT ArrowT (ConT _)) = 0
+countVars (AppT (AppT (TupleT _) _) x) = 1 + countVars x
+countVars (AppT t0 t1) = countVars t0 + countVars t1
+countVars (VarT _) = 1
+countVars (ConT _) = 1
+countVars _ = 0
 
 typeInfo :: DecQ -> Q (Name, [Name], [(Name, Int)], [(Name, [(Maybe Name, Type)])])
 typeInfo m =
