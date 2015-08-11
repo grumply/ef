4b6f5cd157bdbcbe109158de6f891573dad9cbf5
S. M. Hickman <sean@grump.ly>
2015-08-11 16:17:30 -0500

    Comment delta and remove Tape/Computer newtypes.

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -1,10 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 module Evaluation where
 
 import           Pairing
@@ -15,73 +10,18 @@ import           Control.Comonad
 import           Control.Comonad.Trans.Cofree
 
 import           Control.Monad
-import           Control.Monad.Fix
-import           Control.Monad.Trans
 import           Control.Monad.Trans.Free
 
 import           Data.Bifunctor
 import           Data.Coerce
-import           Data.Monoid
 
 import           Prelude hiding (id,(.))
 
-import           Data.Functor.Identity
+type Computer instructions context actions state
+    = CofreeT instructions context (actions state)
 
-instance ComonadCofree Identity Identity where
-  unwrap = Identity
-
-newtype Tape symbols actions result
-  = Tape { s :: FreeT symbols actions result
-         } deriving (Functor,Applicative,Monad)
-
-newtype Computer instructions context actions state
-  = Computer { q0 :: CofreeT instructions context (actions state)
-             } deriving (Functor)
-
-newtype Machine symbols instructions context state actions result
-  = Machine { m :: ( Tape     symbols              actions result
-                   , Computer instructions context actions state
-                   )
-            } deriving Functor
-
--- applicative instance threads a computer through a series of tapes,
--- keeping any changes to the computer upon the evaluation of a tape
--- and ignoring the machine associated with that tape completely.
-instance forall instructions symbols context actions state.
-         ( Pairing instructions symbols
-         , Monad actions
-         , ComonadCofree instructions context
-         ) => Applicative (Machine symbols instructions context state actions)
-  where
-    pure :: a -> Machine symbols instructions context state actions a
-    pure a = Machine (Tape tape,Computer computer)
-      where
-        tape = return a
-        computer = coiterT unwrap (cfix extract)
-
-    (<*>) :: Machine symbols instructions context state actions (a -> b)
-          -> Machine symbols instructions context state actions a
-          -> Machine symbols instructions context state actions b
-    (<*>) (Machine (Tape t,Computer c))
-          (Machine (Tape t',Computer _))
-            = Machine (Tape (ap t t'),Computer c)
-
--- monad instance uses the same initial computer to execute every tape,
--- ignoring any updates to the computer upon the evaluation of a tape.
-instance forall instructions symbols context actions state.
-         ( Pairing instructions symbols
-         , Monad actions
-         , ComonadCofree instructions context
-         ) => Monad (Machine symbols instructions context state actions)
-  where
-    return = pure
-    Machine (t,c) >>= f
-      = let t' = Tape $ do (Computer c',a) <- lift $ delta c t
-                           let Machine (t',_) = f a
-                           s t'
-        in Machine (t',c)
-
-build c = Machine (return (),Computer c)
+type Tape symbols actions result
+  = FreeT symbols actions result
 
 delta :: forall instructions symbols context actions state result.
          (Pairing instructions  symbols
@@ -92,17 +32,24 @@ delta :: forall instructions symbols context actions state result.
       -> actions (Computer instructions context actions state,result)
 delta computer tape = do
 
+  -- coercions to guarantee unwrapping/wrapping performance for computer
   let from = coerce :: CofreeT f w (m a)
                     -> w (CofreeF f (m a) (CofreeT f w (m a)))
       to   = coerce :: w (CofreeF f (m a) (CofreeT f w (m a)))
-                    -> Computer f w m a
+                    -> CofreeT f w (m a)
 
-  state <- extract (q0 computer)
-  symbols <- runFreeT $ s tape
-  case symbols of
-    Free symbols' -> pair delta (fmap coerce $ unwrap $ q0 computer)
-                                (fmap coerce symbols')
-    Pure result -> return
-      (to $ fmap (bimap (const (return state)) id) $ from $ q0 computer,result)
+  state   <- extract computer -- effectfully get current state of computer
+  current <- runFreeT tape    -- effectfully get current symbol on tape
 
-runMachine (Machine (t,c)) = delta c t
+  case current of             -- checking for stop symbol
+
+    Free symbol ->            -- continue symbol
+
+      pair delta              -- use delta to pair instruction table with symbol
+           (unwrap computer)  -- get instruction table
+           symbol             -- continue symbol
+
+    Pure result ->            -- stop symbol
+
+      return                  -- return computer with effects removed and result
+        (to $ fmap (bimap (const (return state)) id) $ from computer,result)
