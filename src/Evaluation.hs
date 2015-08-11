68e1c29e833e46ec5f56c7cef24a02621a527747
grumply <sean@grump.ly>
2015-08-11 18:28:21 -0500

    Improve comments for delta.

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -14,8 +14,7 @@ import           Data.Coerce
 type Computer instructions context  actions state
     = CofreeT instructions context (actions state)
 
-type Tape symbols actions result
-  = FreeT symbols actions result
+type Tape = FreeT
 
 -- | delta executes a Computer and Tape in their corresponding actions monad
 -- by using the pairing instances for their instructions and symbols,
@@ -23,8 +22,9 @@ type Tape symbols actions result
 --
 -- Partial application represents a computer awaiting a tape.
 -- Flipped partial application represents a tape awaiting a computer.
-delta :: (Pairing instructions  symbols
-         ,Comonad context,Monad actions
+delta :: (Pairing instructions symbols
+         ,Comonad context
+         ,Monad actions
          )
       => Computer instructions context actions state
       -> Tape     symbols              actions result
@@ -37,16 +37,11 @@ delta computer tape = do
       to   = coerce :: w (CofreeF f (m a) (CofreeT f w (m a)))
                     -> CofreeT f w (m a)
 
-  state   <- extract computer -- get current state of computer
-                              -- and execute effects from previous
-                              -- instruction and symbol pairing
+  state   <- extract computer -- get current state of computer effectfully
 
-  current <- runFreeT tape    -- get next symbol on tape and execute
-                              -- effects linked to the production of
-                              -- and observation of that symbol
+  current <- runFreeT tape    -- get next symbol on tape effectfully
 
-  case current of             -- checking for stop symbol
-
+  case current of
     Free symbol ->            -- continue symbol
 
       pair delta              -- use delta to pair instruction table with symbol
@@ -56,6 +51,4 @@ delta computer tape = do
     Pure result ->            -- stop symbol
 
       return                  -- return computer with effects removed + result.
-                              -- I believe this is a safe thing to do since
-                              -- `return a >>= k = k a` and `m >>= return = m`
         (to $ fmap (bimap (const (return state)) id) $ from computer,result)
