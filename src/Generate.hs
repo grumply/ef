e68cfa0a92cadf6f44579ce83918ebab105e5f5e
S. M. Hickman <sean@grump.ly>
2015-08-12 13:09:14 -0500

    Remove unused imports and pragmas in Generate.hs.

--- src/Generate.hs
+++ src/Generate.hs
@@ -1,68 +1,19 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE BangPatterns #-}
-module Generate
-  -- (mop,expand,stop,Verbosity(..)
-  -- ,deleteRange, insertRange
-
-  -- )
-  where
-
-import qualified Calypso.Static as C
+module Generate where
 
 import Generate.Cabal
-import Generate.Derives
-import Generate.DSL
 import Generate.DSL.Expand
-import Generate.DSL.Helpers
-import Generate.Module
 import Generate.Monad
-import Generate.Representation
-import Generate.Splice
-import Generate.Splice.Import
-import Generate.Utils
 
-import Control.Applicative
-import Control.Arrow
-import Control.Monad
-
-import qualified Data.Aeson as JSON
-import Data.Char
-import Data.Data
-import Data.Either
 import Data.Function
-import qualified Data.IntMap as IM
 import Data.List
-import Data.Maybe
-import Data.Monoid
 import Data.Ord
-import Data.Typeable
-
-import Debug.Trace
-
-import GHC.Generics
 
 import qualified Language.Haskell.TH as TH
 import qualified Language.Haskell.TH.Syntax as TH
 
-import qualified Product
-import qualified Sum
-
-import System.Exit
 import System.IO
-import System.Directory
 import System.FilePath
-import System.Posix.IO
-import System.Posix.Files
-
-import qualified Data.Map as Map
 
 import Prelude hiding (log)
 
