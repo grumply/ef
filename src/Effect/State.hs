49e0963d455be86bb8ed43e9e8def0a385d9a9f2
grumply <sean@grump.ly>
2015-10-01 00:58:27 -0500

    State effects in terms of modify.

--- src/Effect/State.hs
+++ src/Effect/State.hs
@@ -8,37 +8,29 @@ module Effect.State
 
 import Mop
 
-data State st k
-  = Get (st -> k)
-  | Put st k
-  | Modify (st -> st) k
+data State st k = Modify (st -> st) (st -> k)
 
-data Store st k = Store st (st -> st) k (st -> k) ((st -> st) -> k)
+data Store st k = Store st (st -> k)
 
 instance Pair (Store st) (State st) where
-  pair p (Store st f k _   _   ) (Get    stk  ) = pair p (f st,k) stk
-  pair p (Store _  _ _ stk _   ) (Put    st  k) = pair p stk    (st,k)
-  pair p (Store _  _ _ _   fstk) (Modify f   k) = pair p fstk   (f,k)
+  pair p (Store st k) (Modify stst stk) =
+    let st' = stst st
+    in pair p (st',k st') stk
 
 get :: Has (State st) fs m => Plan fs m st
-get = symbol (Get id)
+get = symbol (Modify id id)
 
 put :: Has (State st) fs m => st -> Plan fs m ()
-put st = symbol (Put st ())
+put st = symbol (Modify (const st) (const ()))
 
 modify :: Has (State st) fs m => (st -> st) -> Plan fs m ()
-modify f = symbol (Modify f ())
+modify f = symbol (Modify f (const ()))
 
 modify' :: Has (State st) fs m => (st -> st) -> Plan fs m ()
 modify' f = do
   st <- get
   put $! f st
 
-store' :: forall st fs m. Uses (Store st) fs m => st -> (st -> st) -> Instruction (Store st) fs m
-store' st f = Store st f pure (instruction . flip store' id) (\f' is ->
-  let Store (new :: st) f0 g p m = view is
-  in instruction (Store new (f' . f0) g p m) is
-  )
-
+{-# INLINABLE store #-}
 store :: Uses (Store st) fs m => st -> Instruction (Store st) fs m
-store x = store' x id
+store st0 = Store st0 (instruction . store)
