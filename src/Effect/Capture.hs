88f03124a7be23678bf578a80fffad1e575eb69c
S. M. Hickman <sean@grump.ly>
2015-10-03 16:03:39 -0500

    Semi-working capture semantics.

--- src/Effect/Capture.hs
+++ src/Effect/Capture.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 module Effect.Capture where
 
 import Mop
@@ -7,23 +8,35 @@ data Capture k' k
   | Recall k' k
 data Reify k' k = Reify (k',k) (k' -> k)
 
-checkpoint :: Monad m => PlanT '[Capture (Cont m a)] m (Cont m a)
-checkpoint = sym (Checkpoint id)
+newtype Cont m a = Cont { recall :: Plan '[Capture (Cont m a)] m a }
 
-recall :: Monad m => Cont m a -> PlanT '[Capture (Cont m a)] m (Cont m a)
-recall plan = sym (Recall plan plan)
+getCC :: (Has (Capture (Cont m a)) fs m)
+           => Plan fs m (Cont m a)
+getCC = symbol (Checkpoint id)
 
 instance Pair (Reify k) (Capture k) where
   pair p (Reify kl _) (Checkpoint k) = pair p kl k
   pair p (Reify _ kr) (Recall k' k) = pair p kr (k',k)
 
-reify :: Uses (Reify (Cont m a)) symbols m => Cont m a -> Instruction (Reify (Cont m a)) symbols m
-reify f = Reify (f,pure) (instr . reify)
+reify :: (Uses (Reify (Cont m a)) is m)
+      => Cont m a -> Instruction (Reify (Cont m a)) is m
+reify f = Reify (f,pure) (instruction . reify)
+
+cont :: Monad m => Cont m a -> m (Instructions '[Reify (Cont m a)] m,a)
+cont c = delta (Instructions $ reify c *:* Empty) (recall c)
+
+test :: Cont IO ()
+test = Cont $ do
+  lift (putStrLn "Test")
+  here :: Cont IO () <- getCC
+  ln <- lift (putStr "Continue? [y/n]: " *> getLine)
+  case ln of
+    "y" -> do
+       lift (putStrLn "Continuing...")
+       recall here
+    _   -> return ()
 
 
-newtype Cont m a = Cont { getCont :: PlanT '[Capture (Cont m a)] m a }
-
-
-
--- cont :: Cont m a -> m (Instructions '[Reify (Cont m a)] m,a)
--- cont c = delta (build $ reify c *:* empty) (getCont c)
+main = do
+  (_,_) <- cont test
+  print ()
