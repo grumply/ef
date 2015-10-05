24bf25264bdf1f4e831f4711ccc12c16d9341dbd
S. M. Hickman <sean@grump.ly>
2015-10-05 14:50:45 -0500

    Minimal type signatures for exception effect.

--- src/Effect/Exception.hs
+++ src/Effect/Exception.hs
@@ -10,26 +10,25 @@ data Handle e k = Handle (e -> k)
 data Catch e k' k = Catch (e -> k') k
 data HandleInjector e k' k = HandleInjector ((e -> k') -> k)
 
-throw :: Has (Throw e) syms m
-      => e -> Plan syms m a
+-- throw :: Has (Throw e) syms m => e -> Plan syms m a
 throw e = symbol (Throw e)
 
-catch :: Has (Catch e (Excepted e m a)) (App e m a) m
-      => (e -> Excepted e m a) -> Plan (App e m a) m ()
+-- catch :: Has (Catch e k) syms m => (e -> k) -> Plan syms m ()
 catch ek = symbol (Catch ek ())
 
-handle :: (Show e,Applicative m) => Instruction (Handle e) instrs syms m a
+-- handle :: Show e => Instruction (Handle e) instrs syms m a
 handle = Handle (error . ("Uncaught exception: " ++) . show)
 
-handler :: (Uses (Handle e) (Ctx e m a) (App e m a) m,Applicative m)
-        => Instruction (HandleInjector e (Excepted e m a)) (Ctx e m a) (App e m a) m a
-handler = HandleInjector (\ep -> modI (push (Handle (\e -> modP ((getExcepted $ ep e) >>)))))
+-- messy type
+handler f = HandleInjector (\ep -> modI (push (Handle (\e -> modP ((f $ ep e) >>)))))
 
 instance Pair (Handle e) (Throw e) where
   pair p (Handle ekk) (Throw e) = p (ekk e) undefined
 
-instance Pair (HandleInjector e (Excepted e m a)) (Catch e (Excepted e m a)) where
+instance Pair (HandleInjector e k) (Catch e k) where
   pair p (HandleInjector hkk) (Catch h k) = p (hkk h) k
+
+--------------------------------------------------------------------------------
 
 type App e m a = '[Throw e,Catch e (Excepted e m a)]
 type Ctx e m a = '[Handle e,HandleInjector e (Excepted e m a)]
@@ -39,7 +38,7 @@ data Bad = Bad deriving Show
 
 -- plan :: String -> Plan (App Bad IO ()) IO ()
 plan nm0 = do
-  catch (\Bad -> Excepted $ return ())
+  catch (\Bad -> Excepted $ return () :: Excepted Bad IO ())
   nm <- lift (putStr "Name: " *> getLine)
   if nm /= nm0
   then lift (putStrLn "Bad!") >> throw Bad
@@ -48,13 +47,9 @@ plan nm0 = do
 
 comp :: Instructions (Ctx Bad IO ()) (App Bad IO ()) IO ()
 comp =
-  let hndl = handle   :: Instruction (Handle Bad) (Ctx Bad IO ()) (App Bad IO ()) IO ()
-      hndlr = handler :: Instruction (HandleInjector Bad (Excepted Bad IO ()))
-                                     (Ctx Bad IO ())
-                                     (App Bad IO ())
-                                     IO
-                                     ()
-  in Instructions $ push hndl $ push hndlr $ unsafeBuild'
+  let hndl = handle
+      hndlr = handler getExcepted
+  in Instructions $ hndl *:* hndlr *:* Empty
 
 main = do
   delta comp (plan "Sean")
