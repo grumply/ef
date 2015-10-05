a3281352b8d7147d8b6eafd93aa1bd1087b40c08
S. M. Hickman <sean@grump.ly>
2015-10-05 14:36:35 -0500

    Working exception handling, but very clunky. Abstraction needed.

--- src/Effect/Exception.hs
+++ src/Effect/Exception.hs
@@ -1,41 +1,60 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds #-}
 module Effect.Exception where
 
 import Mop
 
-data Throw e k' k = Throw e
-data Handle e k' k = Handle (e -> k') k -- rather than simply (e -> k) ?
+data Throw e k = Throw e
+data Handle e k = Handle (e -> k)
 
 data Catch e k' k = Catch (e -> k') k
 data HandleInjector e k' k = HandleInjector ((e -> k') -> k)
 
-throw :: forall e fs m a . Has (Throw e (Plan fs m a)) fs m => e -> Plan fs m a
-throw e = symbol (Throw e :: Throw e (Plan fs m a) a)
+throw :: Has (Throw e) syms m
+      => e -> Plan syms m a
+throw e = symbol (Throw e)
 
-catch :: Has (Catch e (Plan fs m a)) fs m => (e -> Plan fs m a) -> Plan fs m a -> Plan fs m a
-catch ek k = do
-  symbol (Catch ek ())
-  k
+catch :: Has (Catch e (Excepted e m a)) (App e m a) m
+      => (e -> Excepted e m a) -> Plan (App e m a) m ()
+catch ek = symbol (Catch ek ())
 
-handle :: (Show e,Applicative m) => Handle e (Plan fs m a) (Transformation fs gs m a)
-handle = Handle (error . ("Uncaught exception: " ++) . show) pure
+handle :: (Show e,Applicative m) => Instruction (Handle e) instrs syms m a
+handle = Handle (error . ("Uncaught exception: " ++) . show)
 
-handler :: Uses (Handle e (Plan fs m a)) gs fs m
-        => Instruction (HandleInjector e (Plan fs m a)) gs fs m a
-handler = HandleInjector (\ep -> modI (push (Handle (\e -> modP (const (ep e))))))
+handler :: (Uses (Handle e) (Ctx e m a) (App e m a) m,Applicative m)
+        => Instruction (HandleInjector e (Excepted e m a)) (Ctx e m a) (App e m a) m a
+handler = HandleInjector (\ep -> modI (push (Handle (\e -> modP ((getExcepted $ ep e) >>)))))
 
-instance Pair (Handle e k) (Throw e k) where
-  pair p (Handle ek k) (Throw e) = p (ek e) _
+instance Pair (Handle e) (Throw e) where
+  pair p (Handle ekk) (Throw e) = p (ekk e) undefined
 
-instance Pair (HandleInjector e k) (Catch e k) where
+instance Pair (HandleInjector e (Excepted e m a)) (Catch e (Excepted e m a)) where
   pair p (HandleInjector hkk) (Catch h k) = p (hkk h) k
 
--- It seems like every time I want to implement control flow,
--- it requires fully encapsulating the program type.
--- Why is this fundamental?
+type App e m a = '[Throw e,Catch e (Excepted e m a)]
+type Ctx e m a = '[Handle e,HandleInjector e (Excepted e m a)]
+newtype Excepted e m a = Excepted { getExcepted :: Plan (App e m a) m a }
 
+data Bad = Bad deriving Show
 
-{-
-Because, with stacks of monadic effects, the control flow affects everything
-below the control flow effect layer. Similarly, since every effect is on the
-same level in this approach, we must fully encapsulate the entire program type....
--}
+-- plan :: String -> Plan (App Bad IO ()) IO ()
+plan nm0 = do
+  catch (\Bad -> Excepted $ return ())
+  nm <- lift (putStr "Name: " *> getLine)
+  if nm /= nm0
+  then lift (putStrLn "Bad!") >> throw Bad
+  else return ()
+  lift (print "Test")
+
+comp :: Instructions (Ctx Bad IO ()) (App Bad IO ()) IO ()
+comp =
+  let hndl = handle   :: Instruction (Handle Bad) (Ctx Bad IO ()) (App Bad IO ()) IO ()
+      hndlr = handler :: Instruction (HandleInjector Bad (Excepted Bad IO ()))
+                                     (Ctx Bad IO ())
+                                     (App Bad IO ())
+                                     IO
+                                     ()
+  in Instructions $ push hndl $ push hndlr $ unsafeBuild'
+
+main = do
+  delta comp (plan "Sean")
