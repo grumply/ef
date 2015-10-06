4c569acffb99f86edc13ee0c6b38848d4872f8f7
grumply <sean@grump.ly>
2015-10-05 23:29:24 -0500

    Exception handling tied in with Control.Exception. Needs testing.

--- src/Effect/Exception.hs
+++ src/Effect/Exception.hs
@@ -1,55 +1,37 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DataKinds #-}
-module Effect.Exception where
+module Effect.Exception
+  (throw,catch,throwHandler,Throw(..),ThrowHandler(..),Exception(..),SomeException(..))
+  where
 
 import Mop
+import Control.Exception hiding (throw,catch)
 
-data Throw e k = Throw e
-data Handle e k = Handle (e -> k)
+data Throw k = Throw SomeException
+data ThrowHandler k = ThrowHandler k
 
-data Catch e k' k = Catch (e -> k') k
-data HandleInjector e k' k = HandleInjector ((e -> k') -> k)
+-- abstract over this pattern-matching pattern, it seems common and useful.
+scrub :: (Has Throw syms m,Exception e) => (e -> Plan syms m a) -> Plan syms m a -> Plan syms m a
+scrub sub p0 = go p0
+  where
+    go p0 =
+      case p0 of
+        Step syms bp ->
+          case prj syms of
+            Just (Throw se) ->
+              case fromException se of
+                Just x -> sub x
+                Nothing -> Step syms (\b -> go (bp b))
+                -- ^ Look closely here.
+            Nothing -> Step syms (\b -> go (bp b))
+        M mp -> M (fmap go mp)
+        Pure r -> Pure r
 
--- throw :: Has (Throw e) syms m => e -> Plan syms m a
+catch :: (Has Throw syms m,Exception e) => (e -> Plan syms m a) -> Plan syms m a -> Plan syms m a
+catch = scrub
+
 throw e = symbol (Throw e)
 
--- catch :: Has (Catch e k) syms m => (e -> k) -> Plan syms m ()
-catch ek = symbol (Catch ek ())
+throwHandler :: Applicative m => Instruction ThrowHandler instrs syms m a
+throwHandler = ThrowHandler pure
 
--- handle :: Show e => Instruction (Handle e) instrs syms m a
-handle = Handle (error . ("Uncaught exception: " ++) . show)
-
--- messy type
-handler f = HandleInjector (\ep -> modI (push (Handle (\e -> modP ((f $ ep e) >>)))))
-
-instance Pair (Handle e) (Throw e) where
-  pair p (Handle ekk) (Throw e) = p (ekk e) undefined
-
-instance Pair (HandleInjector e k) (Catch e k) where
-  pair p (HandleInjector hkk) (Catch h k) = p (hkk h) k
-
---------------------------------------------------------------------------------
-
-type App e m a = '[Throw e,Catch e (Excepted e m a)]
-type Ctx e m a = '[Handle e,HandleInjector e (Excepted e m a)]
-newtype Excepted e m a = Excepted { getExcepted :: Plan (App e m a) m a }
-
-data Bad = Bad deriving Show
-
--- plan :: String -> Plan (App Bad IO ()) IO ()
-plan nm0 = do
-  catch (\Bad -> Excepted $ return () :: Excepted Bad IO ())
-  nm <- lift (putStr "Name: " *> getLine)
-  if nm /= nm0
-  then lift (putStrLn "Bad!") >> throw Bad
-  else return ()
-  lift (print "Test")
-
-comp :: Instructions (Ctx Bad IO ()) (App Bad IO ()) IO ()
-comp =
-  let hndl = handle
-      hndlr = handler getExcepted
-  in Instructions $ hndl *:* hndlr *:* Empty
-
-main = do
-  delta comp (plan "Sean")
+instance Pair ThrowHandler Throw where
+  pair p (ThrowHandler k) (Throw e) = p k (error $ "Uncaught exception: " ++ show e)
