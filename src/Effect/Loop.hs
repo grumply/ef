6137e221ed8e5b416fcb9f63df14186aff87ca6a
grumply <sean@grump.ly>
2015-10-07 06:03:00 -0500

    Cleaned and refactored loop code.

--- src/Effect/Loop.hs
+++ src/Effect/Loop.hs
@@ -19,32 +19,40 @@ data LoopHandler k = LoopHandler Integer k
 freshScope :: Has Loop fs m => Plan fs m Integer
 freshScope = symbol (FreshScope id)
 
-loop :: forall fs m st a. Has Loop fs m => st -> ((forall b. a -> Plan fs m b) -> (forall b. st -> Plan fs m b) -> st -> Plan fs m a) -> Plan fs m a
+loop :: forall fs m s a. Has Loop fs m
+     => s
+     -> (    (forall b. a -> Plan fs m b)
+          -> (forall b. s -> Plan fs m b)
+          -> s
+          -> Plan fs m a
+        )
+     -> Plan fs m a
 loop s0 x = do
     scope <- freshScope
-    let break a = symbol (Break scope a)
-        continue st = symbol (Continue scope st)
-        loopBody restart st =
-          transform restart scope (x break continue st)
+    let break a      = symbol (Break scope a)
+        continue s   = symbol (Continue scope s)
+        loopBody r s = reifyLoop r scope (x break continue s)
     fix loopBody s0
-  where
-    transform restart scope =
-      mapStep $ \go (Step syms bp) ->
-        case prj syms of
-          Just l ->
-            case l of
-              Continue i s ->
-                if i == scope
-                then restart (unsafeCoerce s)
-                else Step syms (\b -> go (bp b))
-              Break i a ->
-                if i == scope
-                then Pure (unsafeCoerce a)
-                else Step syms (\b -> go (bp b))
-              _ ->   Step syms (\b -> go (bp b))
-          Nothing -> Step syms (\b -> go (bp b))
 
-loops :: Uses LoopHandler gs m => Instruction LoopHandler gs m
+reifyLoop :: Has Loop symbols m
+          => (b -> Plan symbols m a)
+          -> Integer
+          -> Plan symbols m a
+          -> Plan symbols m a
+reifyLoop restart scope =
+  mapStep $ \go (Step syms bp) ->
+    let step = Step syms (\b -> go (bp b))
+        handle (Continue i s)
+          | i == scope = restart (unsafeCoerce s)
+          | otherwise = step
+        handle (Break i a)
+          | i == scope = Pure (unsafeCoerce a)
+          | otherwise = step
+        handle _ = step
+    in maybe step handle (prj syms)
+
+loops :: Uses LoopHandler gs m
+      => Instruction LoopHandler gs m
 loops = LoopHandler 0 $ \fs ->
   let LoopHandler i k = view fs
   in instruction (LoopHandler (succ i) k) fs
