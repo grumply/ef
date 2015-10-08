c8d84020b990b6d5167e156c9c46a6dcf065c5e4
grumply <sean@grump.ly>
2015-10-08 15:39:56 -0500

    Clean up Threading implementation and example.

--- src/Effect/Threading.hs
+++ src/Effect/Threading.hs
@@ -5,8 +5,56 @@ module Effect.Threading where
 import Mop
 
 import Unsafe.Coerce
-
 import Control.Monad
+
+data Thread k
+  = forall fs m a. Thread (Plan fs m a) k
+  | Yield k
+  | Stop
+
+-- round-robin threading
+thread :: (Monad m,Has Thread fs m)
+     => ((forall b. Plan fs m b -> Plan fs m ()) -> Plan fs m () -> Plan fs m a)
+     -> Plan fs m a
+thread x = do
+    transform emptyQueue
+      $ x (\p -> symbol (Thread (p >> symbol Stop) ()))
+          (symbol (Yield ()))
+  where
+    transform q p0 = go p0
+      where
+        go p =
+          case p of
+            Step syms bp ->
+              case prj syms of
+                Just x ->
+                  case x of
+                    Thread child k ->
+                      transform (enqueue (unsafeCoerce child) q) (bp k)
+                    Yield k ->
+                      case dequeue q of
+                        Nothing -> go (bp k)
+                        Just (rest,nxt) ->
+                          transform (enqueue (unsafeCoerce (bp k)) rest) nxt
+                    Stop ->
+                      case dequeue q of
+                        Nothing -> Step syms bp
+                        Just (rest,nxt) -> transform rest nxt
+                Nothing -> Step syms (\b -> go (bp b))
+            M m -> M (fmap go m)
+            Pure r ->
+              case dequeue q of
+                Nothing -> Pure r
+                Just (rest,nxt) ->
+                  transform rest (unsafeCoerce nxt)
+
+instance Pair Threading Thread where
+  pair p (Threading k) Stop = p k undefined
+
+data Threading k = Threading k
+
+threading :: Uses Threading gs m => Instruction Threading gs m
+threading = Threading return
 
 -- amortized constant-time queue
 data Queue = forall fs m a. Queue [Plan fs m a] [Plan fs m a]
@@ -24,53 +72,3 @@ dequeue (Queue [] xs) =
   let stack = reverse xs
   in Just (Queue (tail stack) [],unsafeCoerce (head stack))
 dequeue (Queue xs ys) = Just (Queue (tail xs) ys,unsafeCoerce (head xs))
-
-data Thread k
-  = forall fs m a. Thread (Plan fs m a) k
-  | Yield k
-  | Stop
-
-threaded :: (Monad m,Has Thread fs m)
-         => (   (forall b. Plan fs m b -> Plan fs m ())
-              -> Plan fs m ()
-              -> Plan fs m a
-            )
-         -> Plan fs m a
-threaded x = do
-    transform emptyQueue
-      $ x (\p -> symbol (Thread (p >> symbol Stop) ()))
-          (symbol (Yield ()))
-  where
-    transform q p0 = go p0
-      where
-        go p =
-          case p of
-            Step syms bp ->
-              case prj syms of
-                Just x ->
-                  case x of
-                    Thread child k -> transform (enqueue (unsafeCoerce child) q) (bp k)
-                    Yield k ->
-                      case dequeue q of
-                        Nothing -> go (bp k)
-                        Just (rest,nxt) -> transform (enqueue (unsafeCoerce (bp k)) rest) nxt
-                    Stop ->
-                      case dequeue q of
-                        Nothing -> Step syms bp
-                        Just (rest,nxt) -> transform rest nxt
-                Nothing -> Step syms (\b -> go (bp b))
-            M m -> M (fmap go m)
-            Pure r ->
-              case dequeue q of
-                Nothing -> Pure r
-                Just (rest,nxt) -> transform rest (unsafeCoerce nxt)
-
-
-
-instance Pair Threading Thread where
-  pair p (Threading k) Stop = p k undefined
-
-data Threading k = Threading k
-
-threading :: Uses Threading gs m => Instruction Threading gs m
-threading = Threading return
