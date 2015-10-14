ec89cb7d4e604c9bf0ac42135b5fca473106c15a
grumply <sean@grump.ly>
2015-10-14 00:29:10 -0500

    Add unsafeMIO, unsafePerformMIO, and inlineUnsafePerformMIO.

--- src/Mop/IO.hs
+++ src/Mop/IO.hs
@@ -3,13 +3,24 @@ module Mop.IO where
 import Mop
 import Effect.Exception
 import qualified Control.Exception as Exc
+import System.IO.Unsafe
 
--- this class and module exists to avoid a core dependency on mtl/transformers.
+{-# INLINE inlineUnsafePerformMIO #-}
+inlineUnsafePerformMIO :: IO a -> PlanT fs m a
+inlineUnsafePerformMIO = Pure . unsafePerformIO
+
+{-# NOINLINE unsafePerformMIO #-}
+unsafePerformMIO :: IO a -> PlanT fs m a
+unsafePerformMIO = Pure . unsafePerformIO
 
 class Monad m => MIO m where
+  unsafeMIO :: IO a -> PlanT fs m a
   mio :: Has Throw fs m => IO a -> PlanT fs m a
 
 instance MIO IO where
+  {-# INLINE unsafeMIO #-}
+  unsafeMIO = lift
+  {-# INLINE mio #-}
   mio ioa = do
     ea <- lift $ Exc.try ioa
     case ea of
