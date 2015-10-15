805e9c75bf38de30d58f4230c1706c20cc8ad169
grumply <sean@grump.ly>
2015-10-15 07:05:37 -0500

    Finished Actor system type outline.

--- src/Effect/Actor.hs
+++ src/Effect/Actor.hs
@@ -1,55 +1,82 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ExistentialQuantification #-}
 module Effect.Actor where
-
-import Control.Concurrent
 
 import Mop
 import Mop.IO
+import Effect.Exception
 import Effect.Weave
+
+import Control.Concurrent
+import Data.IORef
 
 type Name = String
 
-data ActorPrims k
-  = FreshScope (Integer -> k)
-  | System k                     -- create an actor system
-  | forall m m' m'' gs fs hs.
-    Pair (Instrs gs) (Symbol fs) =>
-    Supervisor
-      Name
-      (forall x. m' x -> IO x)
-      (InstructionsT gs m')
-      (forall b. PlanT fs m b -> PlanT fs m b)
-      (    Name
-        -- ^ Actor name
-        -> (forall x. m'' x -> IO x)
-        -- ^ lifting function for our actor;
-        --   how to embed the actor into IO.
-        -> InstructionsT hs m''
-        -- ^ Actor object
-        -> (forall b. PlanT hs m' b -> PlanT hs m' b)
-        -- ^ Supervision strategy applied to
-        --   every execution of the actor.
-        -> PlanT gs m' (Name,ThreadId)
-        -- ^ Encapsulated Name, ThreadId and Inbox return
-      )
-      k
-  | New k                        -- create a new supervised actor
-  | Unsupervised k               -- create an unsupervised actor
-  | Stop (Name,ThreadId) k -- stop an actor
+data Actor k
+  = forall e hs is m'.
+    (Pair (Instrs is) (Symbol hs),Has Actor hs m',Exception e)
+    => Supervisor
+         Name
+         (forall x. m' x -> IO x)
+         (forall b. PlanT hs m' b -> (e -> PlanT hs m' b) -> PlanT hs m' b)
+         (InstructionsT is m')
+         k
+  | forall e' m'' js ks.
+    (Pair (Instrs ks) (Symbol js),Has Actor js m'')
+    => Actor
+         Name
+         (forall x. m'' x -> IO x)
+         (forall b. PlanT js m'' b -> (e' -> PlanT js m'' b) -> PlanT js m'' b)
+         (InstructionsT ks m'')
+         k
+  | Stop ((Name,ThreadId) -> k)
 
-data ActorSystem k =
-  ActorSystem k
+data Actors k = Actors (IORef [(Name,ThreadId)]) k
 
--- | system use:
--- >    system $ \supervisor rcv -> do
--- >      -- Creates a scoped system with a function capable of
--- >      -- instantiating a supervisor and a function capable
--- >      -- of reading messages from a some inbox.
--- >      supervisor "someSupervisor" id supervisorObj supervision $ \supervised -> do
--- >        -- Creates a supervisor with the following properties:
--- >        -- Name: `"someSupervisor"`
--- >        -- Lifting of effects: `id` specifies execution in IO and that the
--- >        --                     entire system is executing in IO.
--- >        -- Supervisor: `supervisorObj` is the specific supervisor to be run.
--- >        -- Supervision strategy: `supervision`
--- >        supervised "someActor" $ \rcv -> do
+-- | system is the initialization point for an actor system.
 --
+-- @
+-- system supervisionStrategy0 $ \supervisor -> do
+--   sObj <- createNewSupervisorObjEffectfully
+--   supervisor "someSupervisor" id supervisionStrategy1 sObj $ \actor -> do
+--     aObj <- createNewActorObjEffectfully
+--     actor "someActor" id aObj
+-- @
+system :: forall fs m a.
+          ( Has Actor fs m, MIO m)
+       => (   (    forall e m' hs is. (Pair (Instrs is) (Symbol hs),Has Actor hs m',Exception e)
+                => Name
+                -> (forall x. m' x -> IO x)
+                -> (forall b. PlanT hs m' b -> (e -> PlanT hs m' b) -> PlanT hs m' b)
+                -> InstructionsT is m'
+                -> PlanT hs m' (Name,ThreadId)
+              )
+           -> (    forall e' m' m'' hs js ks.
+                   (Pair (Instrs ks) (Symbol js),Has Actor hs m',Has Actor js m'',Exception e')
+                => Name
+                -> (forall x. m'' x -> IO x)
+                -> (forall b. PlanT js m'' b -> (e' -> PlanT js m'' b) -> PlanT js m'' b)
+                -> InstructionsT ks m''
+                -> PlanT hs m' (Name,ThreadId)
+               )
+            -> PlanT fs m a
+          )
+       -> PlanT fs m a
+system x =
+    system_ $
+      x (\nm lft strat obj -> symbol (Supervisor nm lft strat obj undefined))
+        (\nm lft strat obj -> symbol (Actor nm lft strat obj undefined))
+  where
+    system_ :: PlanT fs m a -> PlanT fs m a
+    system_ p =
+      case p of
+        Step sym bp ->
+          case prj sym of
+            Just x ->
+              case x of
+                Supervisor nm lft strat obj _ -> do
+                  undefined
+                Actor nm lft strat obj _ ->
+                  undefined
+        M m -> M (fmap system_ m)
+        Pure r -> Pure r
