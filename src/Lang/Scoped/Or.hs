74163a9af517a88b7908766fcf266d07a8e8cfdb
S. M. Hickman <sean@grump.ly>
2015-11-09 20:36:31 -0600

    Convert either/or to this/that.

--- src/Lang/Scoped/Or.hs
+++ src/Lang/Scoped/Or.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Lang.Scoped.Or
-    ( Or(..), this, that
+    ( Or(..), isThis, isThat
     , EitherOr(..), eitherOr
     ) where
 
@@ -9,30 +9,23 @@ import Mop.Core
 
 import Lang.Scoped.Exit
 
-import Prelude hiding (either,or)
-
--- Note that the overlap of this module's either with Prelude's is a failure
--- of Haskell, not this library. A future, better, version of Haskell should
--- allow this overlap. As it stands, this module is clunky and Exit should
--- be preferred, sadly.
-
 data a `Or` b = This a | That b
 
-this :: a `Or` b -> Bool
-this (This a) = True
-this _ = False
+isThis :: a `Or` b -> Bool
+isThis (This a) = True
+isThis _ = False
 
-that :: a `Or` b -> Bool
-that (That b) = True
-that _ = False
+isThat :: a `Or` b -> Bool
+isThat (That b) = True
+isThat _ = False
 
 data EitherOr a b fs m = EitherOr
-  { either :: a -> Pattern fs m (a `Or` b)
-  , or :: b -> Pattern fs m (a `Or` b)
+  { this :: a -> Pattern fs m (a `Or` b)
+  , that :: b -> Pattern fs m (a `Or` b)
   }
 
 eitherOr :: Is Exiting fs m => (EitherOr a b fs m -> Pattern fs m (a `Or` b)) -> Pattern fs m (a `Or` b)
 eitherOr f = exits $ \exit -> f EitherOr
-  { either = \a -> exit (This a)
-  , or = \b -> exit (That b)
+  { this = \a -> exit (This a)
+  , that = \b -> exit (That b)
   }
