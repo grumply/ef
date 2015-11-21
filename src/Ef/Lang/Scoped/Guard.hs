ffe74705206f92a653188182993b26d58d39e80b
grumply <sean@grump.ly>
2015-11-21 15:04:09 -0600

    Fully applied new styles to Guard.

--- src/Ef/Lang/Scoped/Guard.hs
+++ src/Ef/Lang/Scoped/Guard.hs
@@ -17,34 +17,42 @@ import Ef.Core
 import Data.Foldable
 import Unsafe.Coerce
 
+
+
 -- | Symbol
 
 data Guarding k
+
     = FreshScope (Int -> k)
 
     | forall a. Choose Int [a] (a -> k)
 
     | Cut Int
 
+
+
 -- | Symbol Module
 
 data Guard fs m =
     Guard
-        {
-          choose
+        { choose
               :: forall f a.
                  Foldable f
-              => f a
-              -> Pattern fs m a
+              => f a -> Pattern fs m a
 
-        , cut :: forall b.
+        , cut
+              :: forall b.
                  Pattern fs m b
         }
+
+
 
 -- | Attribute
 
 data Guardable k =
     Guardable Int k
+
+
 
 -- | Attribute Construct
 
@@ -53,18 +61,22 @@ guarder
     => Attribute Guardable fs m
 guarder =
     Guardable 0 $ \fs ->
+        let
+          Guardable i k =
+              view fs
+        in
+          return $ fs .=
+              Guardable (succ i) k
 
-        let Guardable i k =
-                view fs
 
-        in pure $ fs .=
-               Guardable (succ i) k
 
 -- | Symbol/Attribute Symmetry
 
 instance Symmetry Guardable Guarding where
     symmetry use (Guardable i k) (FreshScope ik) =
         use k (ik i)
+
+
 
 -- | Local Scoping Construct
 
@@ -75,103 +87,134 @@ guards
          -> Pattern fs m a
        )
     -> Pattern fs m (Maybe a)
-guards l = do
+guards l =
+  do
+    scope <- self (FreshScope id)
+    rewrite scope $ l
+        Guard
+            { choose = \foldable ->
+                  let
+                    list = toList foldable
+                  in
+                    self (Choose scope list id)
 
-  scope <- self (FreshScope id)
+            , cut =
+                  self (Cut scope)
+            }
 
-  go scope $
-      l Guard
-          { choose = \as -> self (Choose scope (toList as) id)
-          , cut = self (Cut scope)
-          }
-  where
 
-    go scope p0 =
-        start p0
-      where
 
-        start (Step sym bp) =
-            let
-              ignore =
-                  Step sym (start . bp)
+rewrite
+    :: (Is Guarding fs m)
+    => Int
+    -> Pattern fs m a
+    -> Pattern fs m (Maybe a)
+rewrite scope (Step sym bp) =
+    let
+      ignore =
+          Step sym (rewrite scope . bp)
 
-              check i scoped =
-                  if i == scope then
-                      scoped
-                  else
-                      ignore
-            in
-              case prj sym of
+      check i scoped =
+          if i == scope then
+              scoped
+          else
+              ignore
+    in
+      case prj sym of
 
-                  Just x ->
-                      case x of
+          Just x ->
+              case x of
 
-                          Choose i as _ ->
-                              check i $
-                                  try
-                                      as
-                                      (unsafeCoerce bp)
-                                      (return Nothing)
+                  Choose i as _ ->
+                      check i $
+                          choosing
+                              scope
+                              as
+                              (unsafeCoerce bp)
+                              (return Nothing)
 
-                          _ ->
-                              ignore
-
-                  Nothing ->
-                      Step sym (start . bp)
-
-        start (M m) =
-            M (fmap start m)
-
-        start (Pure result) =
-            return (Just result)
-
-        try [] _ alt =
-            alt
-
-        try (a:as) bp alt =
-            try' as alt bp (bp a)
-
-        try' as alt bp (Step sym bp') =
-            let
-              ignore =
-                  Step sym (try' as alt bp . bp')
-
-              check i scoped =
-                  if i == scope then
-                      scoped
-                  else
-                      ignore
-            in
-              case prj sym of
-
-                  Just x ->
-                      case x of
-
-                          Choose i as' _ ->
-                              check i $
-                                  try
-                                      (unsafeCoerce as')
-                                      (unsafeCoerce bp')
-                                      (try as bp alt)
-
-                          Cut i ->
-                              check i $
-                                  try as bp alt
-
-                          _ ->
-                              ignore
-
-                  Nothing ->
+                  _ ->
                       ignore
 
-        try' as alt bp (M m) =
-            let
-              nextTry = try' as alt bp
-            in
-              M (fmap nextTry m)
+          Nothing ->
+              Step sym (rewrite scope . bp)
 
-        try' _ _ _ (Pure result) =
-            return result
+rewrite scope (M m) =
+    let
+      continue = rewrite scope
+    in
+      M (fmap continue m)
+
+rewrite _ (Pure result) =
+    return (Just result)
+
+
+
+choosing
+    :: (Is Guarding fs m)
+    => Int
+    -> [a]
+    -> (a -> Pattern fs m r)
+    -> Pattern fs m r
+    -> Pattern fs m r
+choosing _ [] _ alt =
+    alt
+
+choosing scope (a:as) bp alt =
+    nestedChoices scope as alt bp (bp a)
+
+
+
+nestedChoices
+    :: (Is Guarding fs m)
+    => Int
+    -> [a]
+    -> Pattern fs m r
+    -> (a -> Pattern fs m r)
+    -> Pattern fs m r
+    -> Pattern fs m r
+nestedChoices scope as alt bp (Step sym bp') =
+    let
+      ignore =
+          Step sym (nestedChoices scope as alt bp . bp')
+
+      check i scoped =
+          if i == scope then
+              scoped
+          else
+              ignore
+    in
+      case prj sym of
+
+          Just x ->
+              case x of
+
+                  Choose i as' _ ->
+                      check i $
+                          choosing
+                              scope
+                              (unsafeCoerce as')
+                              (unsafeCoerce bp')
+                              (choosing scope as bp alt)
+
+                  Cut i ->
+                      check i $
+                          choosing scope as bp alt
+
+                  _ ->
+                      ignore
+
+          Nothing ->
+              ignore
+
+nestedChoices scope as alt bp (M m) =
+    let
+      nextTry = nestedChoices scope as alt bp
+    in
+      M (fmap nextTry m)
+
+nestedChoices _ _ _ _ (Pure result) =
+    return result
 
 -- | Inlines
 
