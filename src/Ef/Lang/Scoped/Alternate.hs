3d95c1b2003413bd9e9fcd555bad03abbc071445
S. M. Hickman <sean@grump.ly>
2015-11-23 05:37:24 -0600

    Start of rewrite of alternate. Added Await primitive, Operation primitive, and Query method.

--- src/Ef/Lang/Scoped/Alternate.hs
+++ src/Ef/Lang/Scoped/Alternate.hs
@@ -12,24 +12,74 @@ module Ef.Lang.Scoped.Alternate
     , alternator
     ) where
 
+
+
 import Ef.Core
 import Ef.Data.Queue
 
+import Data.IORef
 import Unsafe.Coerce
 
 
 
 -- | Symbol
 
-data Alternating k =
+data Operation a
+  where
 
-      forall fs m a. Fork Int (Pattern fs m a)
+    Operation
+        :: Int
+        -> IORef a
+        -> Operation a
 
-    | forall fs m a. Atomically Int (Pattern fs m a)
 
-    | Stop Int
 
-    | FreshScope (Int -> k)
+data Await
+  where
+
+    Atomically
+        :: Await
+
+    Concurrently
+        :: Await
+
+
+
+data Alternating k
+  where
+
+    FreshScope
+        :: (Int -> k)
+        -> Alternating k
+
+    Fork
+        :: Int
+        -> Pattern fs m a
+        -> (Operation a -> k)
+        -> Alternating k
+
+    Await
+        :: Int
+        -> Await
+        -> Operation a
+        -> (a -> k)
+        -> Alternating k
+
+    Atomic
+        :: Int
+        -> Pattern fs m a
+        -> Alternating k
+
+    Stop
+        :: Int
+        -> Operation a
+        -> (a -> k)
+        -> Alternating k
+
+    Finished
+        :: Operation a
+        -> (Bool -> k)
+        -> Alternating k
 
 
 
@@ -38,8 +88,10 @@ data Alternating k =
 data Alternate fs m =
     Alternate
         { alt
-              :: Pattern fs m ()
-              -> Pattern fs m ()
+              :: Pattern fs m a
+              -> Pattern fs m (Operation a)
+
+        ,
 
         , atomically
               :: forall b.
@@ -93,212 +145,127 @@ alternates
        )
     -> Pattern fs m a
 alternates f =
-  do
-    scope <- self (FreshScope id)
-    let
-      alternate =
+    do
+      scope <- self (FreshScope id)
+      rewrite scope emptyQueue $ f
           Alternate
-              { alt =
-                    \p ->
-                        self $ Fork scope $
-                            do
-                              p
-                              self (Stop scope)
-              , atomically =
-                    \p ->
-                        self (Atomically scope p)
-              }
+                { alt =
+                      \p ->
+                          self $ Fork scope $
+                              do
+                                p
+                                self (Stop scope)
+                , atomically =
+                      \p ->
+                          self (Atomically scope p)
+                }
 
-    rewrite scope (f alternate)
 
 
 
 rewrite
     :: Is Alternating fs m
     => Int
-    -> Pattern fs m a
-    -> Pattern fs m a
-rewrite _ (Pure r) =
-    return r
-
-rewrite scope (M m) =
-    let
-      continue = rewrite scope
-    in
-      M (fmap continue m)
-
-rewrite scope (Step sym bp) =
-    let
-      ignore =
-          Step sym (rewrite scope . bp)
-
-      check i scoped =
-          if i == scope then
-              scoped
-          else
-              ignore
-
-    in
-      case prj sym of
-
-          Just x ->
-              case x of
-
-                  Fork i child ->
-                      check i $
-                          let
-                            result =
-                                unsafeCoerce ()
-
-                            newRunQueue =
-                              enqueue (unsafeCoerce child) emptyQueue
-
-                          in
-                            rooted scope newRunQueue (bp result)
-
-                  Atomically i atom ->
-                      check i $
-                          do
-                            b <- rewrite scope (unsafeCoerce atom)
-                            let
-                              continue =
-                                  bp (unsafeCoerce b)
-
-                            rewrite scope continue
-
-                  Stop i ->
-                      check i $
-                          return (unsafeCoerce ())
-
-                  _ ->
-                      ignore
-
-          Nothing ->
-              ignore
-
-
-
-rooted
-    :: Is Alternating fs m
-    => Int
     -> Queue (Pattern fs m a)
     -> Pattern fs m a
     -> Pattern fs m a
-rooted scope rest (Pure r) =
-    case dequeue rest of
-
-      Nothing ->
-          return r
-
-      Just (rest',nxt) ->
-          do
-            _ <- rooted scope rest' nxt
-            return r
-
-rooted scope rest (M m) =
-    M $ do
-          p' <- m
-          handleQueue p'
+rewrite scope =
+    withQueue
   where
 
-    handleQueue continue =
-        case dequeue rest of
+    withQueue queue =
+        go
+      where
 
-            Nothing ->
-                return (rewrite scope continue)
+        -- Returned in root since alternates end in Stop.
+        go (Pure r) =
+            case dequeue queue of
 
-            Just (newRest,next) ->
-                let
-                  newRunQueue =
-                      enqueue (unsafeCoerce continue) newRest
+                -- All forks completed; return.
+                Nothing ->
+                    return r
 
-                in
-                  newRunQueue `seq` return $
-                      rooted scope newRunQueue next
-
-rooted scope rest (Step sym continue) =
-    let
-      ignore =
-          Step sym (rooted scope rest . continue)
-
-      check i scoped =
-          if i == scope then
-              scoped
-          else
-              ignore
-
-    in
-      case prj sym of
-
-        Just x ->
-          case x of
-
-            Fork i child ->
-               check i $
-                   let
-                     newRunQueue =
-                         enqueue (unsafeCoerce child) rest
-
-                     newContinue =
-                         continue (unsafeCoerce ())
-
-                   in
-                     rooted scope newRunQueue newContinue
-
-            Atomically i atom ->
-                check i $
+                -- Finish running forks and then return.
+                Just (newQueue,next) ->
                     do
-                      let
-                        newContinue value =
-                            continue (unsafeCoerce value)
+                      _ <- withQueue newQueue next
+                      return r
 
-                        newRunQueue newRest value =
-                            enqueue (newContinue value) newRest
+        -- Monadic actions are alternated the same as Steps.
+        go (M m) =
+            case dequeue queue of
 
-                      b <- rewrite scope (unsafeCoerce atom)
-                      case dequeue rest of
+                Nothing ->
+                    M (fmap go m)
 
-                          Nothing ->
-                              rewrite scope (continue b)
+                Just (newQueue,next) ->
+                    let
 
-                          Just (newRest,nxt) ->
-                              rooted scope (newRunQueue newRest b) nxt
+                      newRunQueue continue =
+                          enqueue (unsafeCoerce continue) newQueue
 
-            Stop i ->
-                check i $
-                    case dequeue rest of
+                    in
+                      do
+                        continue <- m
+                        withQueue (newRunQueue continue) next
 
-                        Nothing ->
-                            return (unsafeCoerce ())
-
-                        Just (newRest,next) ->
-                            rooted scope newRest next
-
-            _ ->
-                ignore
-
-        Nothing ->
+        go (Step sym bp) =
             let
-              newContinue value =
-                  case dequeue rest of
+              ignore =
+                  Step sym (go . bp)
 
-                      Nothing ->
-                          rewrite scope (continue value)
-
-                      Just (newRest,nxt) ->
-                          let
-                            newRunQueue =
-                                enqueue (continue value) newRest
-
-                          in
-                            rooted scope newRunQueue nxt
+              check i scoped =
+                  if i == scope then
+                      scoped
+                  else
+                      ignore
 
             in
-              Step sym newContinue
+              case prj sym of
+
+                  Just x ->
+                      case x of
+
+                          Fork i child ->
+                              check i $
+                                  let
+                                    result =
+                                        unsafeCoerce ()
+
+                                    newQueue =
+                                        enqueue (unsafeCoerce child) queue
+
+                                    continue =
+                                        bp result
+
+                                  in
+                                    withQueue newQueue continue
+
+                          Atomically i atom ->
+                              check i $
+                                  do
+                                    b <- unsafeCoerce atom
+                                    let
+                                      continue b =
+                                          bp b
+
+                                    go continue
+
+                          Stop i ->
+                              check i $
+                                  case
+
+                          _ ->
+                              ignore
+
+                  Nothing ->
+                      ignore
 
 
 
 -- | Inlines
 
+{-# INLINE rooted #-}
+{-# INLINE rewrite #-}
 {-# INLINE alternator #-}
 {-# INLINE alternates #-}
