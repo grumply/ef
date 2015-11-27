b5235402820e2512a6f4d0cf6bb1176be284a755
S. M. Hickman <sean@grump.ly>
2015-11-27 02:35:31 -0600

    Add IfThenElse to cabal exports.

--- src/Ef/Lang/IfThenElse.hs
+++ src/Ef/Lang/IfThenElse.hs
@@ -14,29 +14,9 @@ import Ef.Core
 import Control.Monad
 import Data.Binary
 import Data.IORef
-import Prelude
 import System.IO.Unsafe
 
 
-
--- This rebound if-then-else gives access to both branches of the conditional
--- for the purposes of DSL optimization. This is extremely experimental and
--- incurs a reasonably large overhead as well as being extremely unsafe and
--- unreliable. An other, safer, way to do this would be exceptionally welcome.
-
--- The reasoning behind this approach is thus: When analyzing a DSL, we need
--- to create the data structure representing the Pattern without actually
--- executing any effects and binding any variables. Instead, we fill all holes
--- with undefined values and use reallyUnsafePtrEquality# to find use-sites on
--- a per-case basis. If we use the built-in if-then-else, we'll end up requiring
--- the Bool to be fully evaluated, but what if that Bool depends upon a bound
--- variable in the Pattern? It might very well be a value we initialized as
--- undefined for static analysis. That works out if we're making an optimization
--- pass of the Pattern dynamically, but what if we want to optimize the Pattern
--- statically? That's where this rebound if-then-else comes in: we put each
--- branch of the conditional in an IORef and use unsafePerformIO to allow
--- analysis of each branch independently. So the hope is for the ability to do
--- static branch prediction as well as static independent branch optimization.
 
 data ITEing k
   where
@@ -56,6 +36,8 @@ data ITEable k
         :: k
         -> ITEable k
 
+
+
 instance Uses ITEable gs m
     => Binary (Attribute ITEable gs m)
   where
@@ -66,6 +48,8 @@ instance Uses ITEable gs m
 
     put _ =
         pure ()
+
+
 
 ifThenElse
     :: Is ITEing fs m
