cdb3eeaaefd4e71ff6b0d344dd14ed04a9d7efc8
grumply <sean@grump.ly>
2015-12-18 21:56:47 -0600

    Added external heap library test for benchmarking. Converted API to IO-based for reuse.

--- src/Ef/Data/MinHeap.hs
+++ src/Ef/Data/MinHeap.hs
@@ -1,35 +1,87 @@
+-- | -- DO NOT USE --
+-- This is abhorrent and disgusting Haskell. These dragons are mutated and move orthogonally; expect nothing predictable.
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Control.Monad
-import Control.Monad.ST
+import Data.List (intersperse,unfoldr)
 
-import Data.List (intersperse)
+import Data.Array.IO
+import Data.Array.Base
+import Data.Array.Unsafe
+import Data.IORef
+import System.IO.Unsafe
+import Debug.Trace
 
-import GHC.Arr
+import qualified Data.Heap as Heap
 
+main' = do
+    let
+        heap = Heap.fromAscList [1..10000000] :: Heap.MinHeap Integer
+
+    print =<< (extractAll heap)
+    where
+        extractAll =
+            go 0 0
+            where
+                go !prev !n !heap =
+                    case Heap.view heap of
+
+                        Just (min,newHeap) -> 
+                            if min > prev then
+                                go min (n + 1) newHeap
+                            else
+                                do
+                                    print $ show min ++ " /> " ++ show prev
+                                    return 0
+                        Nothing ->
+                            return n
 
 main = do
+    currentSize <- newIORef 10000000
+    maxSize <- newIORef 10000000
+    minHeap <- newListArray (0,10000000) (undefined:[1..10000000 :: Integer])
     let
+
         heap =
-            new [1..5000000 :: Int]
+            Heap currentSize maxSize minHeap
+    
+    print =<< extractAll heap
+    where
+        extractAll heap = 
+            go 0 0
+            where
+                go !prev !n =
+                    do
+                        currentMin <- extractMin heap
+                        case currentMin of
 
-    print (fmap fst (extractMin heap))
-    print (fmap fst (extractMin ))
+                            Nothing -> 
+                                return n
 
-
+                            Just min ->
+                                if min > prev then
+                                    go min (n + 1)
+                                else 
+                                    do
+                                        print $ show min ++ " /> " ++ show prev
+                                        return 0
 
 data MinHeap a =
     Heap
         {
-          stats
-              :: {-# UNPACK #-} !(Array Int Int)
+          currentSize
+              :: {-# UNPACK #-} !(IORef Int)
+              
+        , maxSize
+              :: {-# UNPACK #-} !(IORef Int)
 
         , minHeap
-              :: {-# UNPACK #-} !(Array Int a)
+              :: {-# UNPACK #-} !(IOArray Int a)
 
         }
 
@@ -38,11 +90,11 @@ data MinHeap a =
 instance Show a => Show (MinHeap a)
     where
 
-        show (Heap stats minHeap) =
+        show Heap{..} =
             "Heap"
                 ++ "  {"
-                       ++ "\n\t\tcurrentSize: " ++ show (unsafeAt stats 0)
-                       ++ "\n\t\tmaxSize: " ++ show (unsafeAt stats 1)
+                       ++ "\n\t\tcurrentSize: " ++ show (unsafePerformIO $ readIORef currentSize)
+                       ++ "\n\t\tmaxSize: " ++ show (unsafePerformIO $ readIORef maxSize)
                        ++ "\n\t\tminHeap: [" ++ concat (intersperse "," showMinHeap) ++ "]\n"
                 ++ "  }"
             where
@@ -52,12 +104,12 @@ instance Show a => Show (MinHeap a)
                     where
 
                         go index
-                            | index > currentSize =
+                            | index > (unsafePerformIO $ readIORef currentSize) =
                                   []
                             | otherwise =
                                   let
                                       valueAtIndex =
-                                          unsafeAt minHeap index
+                                          unsafePerformIO $ readArray minHeap index
 
                                   in
                                       show valueAtIndex : go (index + 1)
@@ -92,7 +144,7 @@ heightToSize =
 -- | Create an empty min-heap.
 empty
     :: Ord a
-    => MinHeap a
+    => IO (MinHeap a)
 
 empty =
     sizedEmpty 2
@@ -103,202 +155,202 @@ empty =
 sizedEmpty
     :: Ord a
     => Height
-    -> MinHeap a
+    -> IO (MinHeap a)
 
-sizedEmpty (heightToSize -> maxSize) =
-    let
-        currentSize =
-            0
-            
-        stats =
-            unsafeArray' (0,1) 2 [(0,currentSize),(1,maxSize)]
+sizedEmpty height =
+    do
+        let
+            size =
+                heightToSize height
 
-        minHeap =
-            listArray (0,maxSize) []
-
-    in
-        Heap{..}
+        currentSize <- newIORef 0
+        maxSize <- newIORef size
+        minHeap <- newArray_ (0,size)
+        return Heap{..}
 
 
 
 -- | Create a new min-heap from the given size and list, unsafely.
-unsafeNew
+new
     :: Ord a
     => [a]
-    -> MinHeap a
+    -> IO (MinHeap a)
 
-unsafeNew as =
-    let
-        currentSize =
-            length as
+new as =
+    do
+        let
+            count =
+                length as
 
-        maxSize =
-            heightToSize (sizeToHeight currentSize)
-        
-        stats =
-            unsafeArray' (0,1) 2 [(0,currentSize),(1,maxSize)]
+            size =
+                heightToSize (sizeToHeight count)
 
-        elements =
-            zip [1..currentSize] as
+            elements =
+                as
 
-        minHeap =
-            unsafeArray' (0,maxSize) (succ maxSize) elements
+        currentSize <- newIORef count
+        maxSize <- newIORef size
+        minHeap <- newListArray (0,size) (undefined:elements)
 
-    in
         sort Heap{..}
 
 
 
 isEmpty
-    :: Ord a
-    => MinHeap a
-    -> Bool
+    :: MinHeap a
+    -> IO Bool
 
 isEmpty Heap{..} =
-    (unsafeAt stats 0) == 0
+    (== 0) <$> readIORef currentSize
 
 
 
-unsafeSort
+isFull
+    :: MinHeap a
+    -> IO Bool
+
+isFull Heap{..} =
+    do
+        currentSize <- readIORef currentSize
+        maxSize <- readIORef maxSize
+        return (currentSize == maxSize)
+
+
+
+grow
+    :: MinHeap a
+    -> IO (MinHeap a)
+
+grow Heap{..} =
+    undefined
+
+
+
+
+sort
     :: Ord a
     => MinHeap a
-    -> MinHeap a
+    -> IO (MinHeap a)
 
-unsafeSort Heap{..} = Heap {..}
-    -- runST $
-    --     do
-    --         undefined
+sort Heap {..} = 
+    return Heap {..}
 
 
 
-unsafeViewMin
+viewMin
+    :: MinHeap a
+    -> IO (Maybe a)
+
+viewMin Heap{..} =
+    do
+        currentSize <- readIORef currentSize
+        if currentSize == 0 then
+            return Nothing
+        else
+            do
+                min <- unsafeRead minHeap 1
+                return (Just min)
+
+
+
+extractMin
+    :: (Show a, Ord a)
+    => MinHeap a
+    -> IO (Maybe a)
+
+extractMin Heap{..} =
+    do
+        curSize <- readIORef currentSize
+        case curSize of
+
+            0 ->
+                return Nothing
+
+            1 ->
+                do
+                    min <- unsafeRead minHeap 1
+                    writeIORef currentSize 0
+                    return (Just min)
+
+            _ ->
+                do
+                    min <- unsafeRead minHeap 1
+                    largest <- unsafeRead minHeap curSize
+                    unsafeWrite minHeap 1 largest
+                    sink curSize largest minHeap
+                    let
+                        newCurSize =
+                            curSize - 1
+                    newCurSize `seq`
+                        writeIORef currentSize newCurSize
+                    return (Just min)
+
+{-# INLINE sink #-}
+sink
     :: Ord a
-    => MinHeap a
-    -> Maybe a
-
-unsafeViewMin Heap{..} =
-    if (unsafeAt stats 0) == 0 then
-        Nothing
-    else
-        Just (unsafeAt minHeap 1)
-
-
-
-unsafeExtractMin
-    :: forall a.
-       (Ord a,Show a)
-    => MinHeap a
-    -> Maybe (a,MinHeap a)
-
-unsafeExtractMin Heap{..} =
-    --trace (show $ Heap {..}) $
-    case unsafeAt stats 0 of
-
-        0 ->
-            Nothing
-
-        1 ->
-            runST $
-                do
-                    arr <- unsafeThawSTArray minHeap
-                    min <- unsafeReadSTArray arr 1
-                    newMinHeap <- unsafeFreezeSTArray arr
-                    let
-                        result =
-                            Just (min,newHeap)
-
-                    return result
-
-        _ ->
-            runST $
-                do
-                    let
-                        !currentSize =
-                            unsafeAt stats 0
-                            
-                        !maxSize =
-                            unsafeAt stats 1
-                            
-                    arr <- unsafeThawSTArray minHeap
-                    min <- unsafeReadSTArray arr 1
-                    largest <- unsafeReadSTArray arr currentSize
-                    unsafeWriteSTArray arr 1 largest
-                    sink currentSize largest arr
-                    newMinHeap <- unsafeFreezeSTArray arr
-                    let
-                        newHeap =
-                            Heap (currentSize - 1) maxSize newMinHeap
-
-                        result =
-                            Just (min,newHeap)
-
-                    return result
+    => Int
+    -> a
+    -> IOArray Int a
+    -> IO ()
+    
+sink curSize largest minHeap =
+    go 1
     where
+    
+        {-# INLINE go #-}
+        go !index =
+            do
+                let
+                    leftIndex =
+                        index * 2
 
-        sink
-            :: Int
-            -> a
-            -> STArray s Int a
-            -> ST s ()
+                    hasLeft =
+                        leftIndex <= curSize
 
-        sink currentSize largest arr =
-            go 1
-            where
+                    rightIndex =
+                        leftIndex + 1
 
-                go index =
+                    hasRight =
+                        rightIndex <= curSize
+
+                    readLeft =
+                        unsafeRead minHeap leftIndex
+
+                    readRight =
+                        unsafeRead minHeap rightIndex
+
+                    current =
+                        (largest,index)
+
+                smaller@(small,smallIndex) <-
+                    if hasLeft then
+                        do
+                            left <- readLeft
+                            return $
+                                if left < largest then
+                                    (left,leftIndex)
+                                else
+                                    current
+
+                    else
+                        return current
+                (smallest,smallestIndex) <-
+                    if hasRight then
+                        do
+                            right <- readRight
+                            return $
+                                if right < small then
+                                    (right,rightIndex)
+                                else
+                                    smaller
+                    else
+                        return smaller
+
+                when (smallestIndex /= index) $
                     do
-                        let
-                            leftIndex =
-                                index * 2
-
-                            hasLeft =
-                                leftIndex <= currentSize
-
-                            rightIndex =
-                                leftIndex + 1
-
-                            hasRight =
-                                rightIndex <= currentSize
-
-                            readLeft =
-                                unsafeReadSTArray arr leftIndex
-
-                            readRight =
-                                unsafeReadSTArray arr rightIndex
-
-                            current =
-                                (largest,index)
-
-                        smaller@(small,smallIndex) <-
-                            if hasLeft then
-                                do
-                                    left <- readLeft
-                                    return $
-                                        if left < largest then
-                                            (left,leftIndex)
-                                        else
-                                            current
-
-                            else
-                                return current
-
-                        (smallest,smallestIndex) <-
-                            if hasRight then
-                                do
-                                    right <- readRight
-                                    return $
-                                        if right < small then
-                                            (right,rightIndex)
-                                        else
-                                            smaller
-                            else
-                                return smaller
-
-                        when (smallestIndex /= index) $
-                            do
-                                unsafeWriteSTArray arr smallestIndex largest
-                                unsafeWriteSTArray arr index smallest
-                                go smallestIndex
+                        unsafeWrite minHeap smallestIndex largest
+                        unsafeWrite minHeap index smallest
+                        go smallestIndex
 
 
 
@@ -308,18 +360,22 @@ insert
     :: Ord a
     => a
     -> MinHeap a
-    -> MinHeap a
+    -> IO (MinHeap a)
 
-insert new Heap{..} =
-    undefined
-
+insert new heap =
+    do
+        full <- isFull heap
+        if full then
+            undefined
+        else
+            undefined
 
 
 bubble
     :: Ord a
     => Int
-    -> STArray s Int a
-    -> ST s ()
+    -> IOArray Int a
+    -> IO ()
 
 bubble index arr =
     undefined
@@ -327,10 +383,10 @@ bubble index arr =
 
 {-# INLINE heightToSize #-}
 {-# INLINE sizeToHeight #-}
-{-# INLINE unsafeExtractMin #-}
+{-# INLINE extractMin #-}
 {-# INLINE insert #-}
 {-# INLINE bubble #-}
-{-# INLINE unsafeNew #-}
-{-# INLINE unsafeViewMin #-}
+{-# INLINE new #-}
+{-# INLINE viewMin #-}
 {-# INLINE empty #-}
 {-# INLINE sizedEmpty #-}
