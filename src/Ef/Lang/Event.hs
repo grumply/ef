cafbb9217e01e5efebaaa8013e8f70a00905418b
S. M. Hickman <sean@grump.ly>
2016-01-28 21:36:10 -0600

    Skeleton implementation of event system complete.

--- src/Ef/Lang/Event.hs
+++ src/Ef/Lang/Event.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
@@ -9,89 +11,327 @@ module Ef.Lang.Event
 
 import Ef.Core
 import Ef.Core.Narrative
+import Ef.Lang.Knot
 import Ef.Lang.IO
 
-import Data.Typeable
+import Control.Monad
+import Data.IORef
 import Unsafe.Coerce
+import System.IO.Unsafe
 
 
-data Events lexicon environment
+data Reactor lexicon environment event =
+    Reactor
+        {
+          become
+              :: (event -> Narrative lexicon environment ())
+              -> Narrative lexicon environment ()
+
+        , die
+              :: Narrative lexicon environment ()
+
+        }
+
+
+
+data Signal lexicon environment event
     where
 
-        Vacuous
-            :: Events lexicon environment
-
-        Culture
-            :: Typeable a
-            => [Behavior a lexicon environment]
-            -> Events lexicon environment
-            -> Events lexicon environment
+        Signal
+            :: !(IORef event)
+            -> !(IORef [event -> Narrative lexicon environment ()])
+            -> Signal lexicon environment event 
 
 
 
-data Behavior event lexicon environment
-    where
+construct
+    :: event
+    -> Signal lexicon environment event
 
-        Behavior
-            :: (Event lexicon environment -> event -> Narrative lexicon environment ())
-            -> Behavior event lexicon environment
-
-
-dispatch
-    :: forall event lexicon environment.
-       (Typeable event,Typeable environment,Typeable lexicon,Monad environment)
-    => event
-    -> Events lexicon environment
-    -> Narrative lexicon environment ()
-
-dispatch event =
-    go
-    where
-
-        behavior = typeOf (undefined :: event -> Narrative lexicon environment ())
-
-        go Vacuous =
-            return ()
-
-        go (Culture behaviors es) =
-            case cast behaviors :: Maybe [Behavior event lexicon environment] of
-
-                Just bs ->
-                    run bs
-
-                Nothing ->
-                    go es
-
-        run [] =
-            return ()
-
-        run (Behavior b:bs) =
-            do
-                b event
-                run bs
-
-
-main =
+construct event =
     let
-        chb1 ch = io (putStr [ch])
-        chb2 ch = io (putStrLn [ch])
+        !current =
+            unsafePerformIO $ newIORef event
 
-        ib1 i = if i == (0 :: Int) then io (print i) else return ()
-        ib2 i = if i == (1 :: Int) then io (print i) else return ()
-
-        c1 = [Behavior chb1,Behavior chb2]
-
-        c2 = [Behavior ib1,Behavior ib2]
-
-        events = Culture c1 (Culture c2 Vacuous)
-
-        obj = Object Empty
+        !behaviors =
+            unsafePerformIO $ newIORef []
 
     in
-        do
-            obj $.
-                (do
-                    { dispatch 'a' events
-                    ; dispatch (1 :: Int) events
-                    }
-                )
+        Signal current behaviors
+
+
+
+merge_
+    :: Monad environment
+    => (Signal lexicon environment event -> event -> Narrative lexicon environment ())
+    -> Signal lexicon environment event
+    -> Signal lexicon environment event
+    -> Signal lexicon environment event
+
+merge_ triggerMethod (Signal current0 behaviors0) (Signal current1 behaviors1) =
+    let
+        !initial =
+            unsafePerformIO $ readIORef current0
+
+        !current =
+            unsafePerformIO $ newIORef initial
+
+        !behaviors =
+            unsafePerformIO $ newIORef []
+
+        !signal =
+            Signal current behaviors
+
+        addBehaviorLeft =
+            unsafePerformIO $ modifyIORef behaviors0 $ \bs ->
+                let
+                    newBehavior event =
+                        triggerMethod signal event
+
+                in
+                    newBehavior:bs
+
+        addBehaviorRight =
+            unsafePerformIO $ modifyIORef behaviors1 $ \bs ->
+                let
+                    newBehavior event =
+                        triggerMethod signal event
+
+                in
+                    newBehavior:bs
+
+    in
+        addBehaviorLeft `seq` addBehaviorRight `seq` signal
+
+
+
+mapSignal_
+    :: Monad environment
+    => (Signal lexicon environment b -> b -> Narrative lexicon environment ())
+    -> (a -> b)
+    -> Signal lexicon environment a
+    -> Signal lexicon environment b
+
+mapSignal_ triggerMethod f (Signal current0 behaviors0) =
+    let
+        !initial =
+            f $ unsafePerformIO $ readIORef current0
+
+        !current = 
+            unsafePerformIO $ newIORef initial
+
+        !behaviors =
+            unsafePerformIO $ newIORef []
+
+        !signal =
+            Signal current behaviors
+
+        addBehavior =
+            unsafePerformIO $ modifyIORef behaviors0 $ \bs ->
+                let
+                    newBehavior event =
+                        triggerMethod signal (f event)
+
+                in
+                    newBehavior:bs
+
+    in
+        addBehavior `seq` signal
+
+
+
+filterSignal_
+    :: Monad environment
+    => (Signal lexicon environment a -> a -> Narrative lexicon environment ())
+    -> (a -> Bool)
+    -> a
+    -> Signal lexicon environment a
+    -> Signal lexicon environment a
+
+filterSignal_ triggerMethod predicate initial (Signal _ behaviors0) =
+    let
+        !current =
+            unsafePerformIO $ newIORef initial
+
+        !behaviors =
+            unsafePerformIO $ newIORef []
+
+        !signal =
+            Signal current behaviors
+
+        addBehavior =
+            unsafePerformIO $ modifyIORef behaviors0 $ \bs ->
+                let
+                    newBehavior event =
+                        if predicate event then
+                            triggerMethod signal event
+                        else
+                            return ()
+
+                in
+                    newBehavior:bs
+
+    in
+        addBehavior `seq` signal
+
+
+
+behavior_
+    :: Monad environment
+    => Signal lexicon environment event
+    -> (event -> Narrative lexicon environment ())
+    -> Narrative lexicon environment ()
+
+behavior_ (Signal _ behaviors) newBehavior =
+    let
+        updateBehaviors =
+            unsafePerformIO $ modifyIORef behaviors (newBehavior:)
+
+    in
+        updateBehaviors `seq` return ()
+
+
+
+trigger_
+    :: Monad environment
+    => Signal lexicon environment event
+    -> event
+    -> Narrative lexicon environment ()
+
+trigger_ (Signal current behaviors) event =
+    let
+        !writeCurrent =
+            unsafePerformIO $ writeIORef current event
+
+        !behavior =
+            sequence_ $ map ($ event) $
+                unsafePerformIO $ readIORef behaviors
+
+    in
+        writeCurrent `seq` behavior
+
+
+
+data Event lexicon environment =
+    Event
+        {
+          trigger
+              :: forall event.
+                 Signal lexicon environment event
+              -> event
+              -> Narrative lexicon environment ()
+
+        , behavior
+              :: forall event.
+                 Signal lexicon environment event
+              -> (Reactor lexicon environment event -> event -> Narrative lexicon environment ())
+              -> Narrative lexicon environment ()
+
+        , merge
+              :: forall event.
+                 Signal lexicon environment event
+              -> Signal lexicon environment event
+              -> Signal lexicon environment event
+
+        , mapSignal
+              :: forall a b.
+                 (a -> b)
+              -> Signal lexicon environment a
+              -> Signal lexicon environment b
+
+        , filterSignal
+              :: forall a.
+                 (a -> Bool)
+              -> a
+              -> Signal lexicon environment a
+              -> Signal lexicon environment a
+        }
+
+
+
+data Action lexicon environment
+    where
+
+        Trigger
+            :: Signal lexicon environment event
+            -> event
+            -> Action lexicon environment
+
+        Become
+            :: (event -> Narrative lexicon environment ())
+            -> Action lexicon environment
+
+        Die
+            :: Action lexicon environment
+
+
+
+event
+    :: forall lexicon environment a.
+       ( Monad environment
+       , Knows Knots lexicon environment
+       )
+    => (Event lexicon environment -> Narrative lexicon environment a)
+    -> Narrative lexicon environment a
+
+event loop =
+    let
+        ev up =
+            Event
+                {
+                  trigger =
+                      \signal event ->
+                          up (Trigger signal event)
+
+                , behavior =
+                      \signal b ->
+                          let
+                              reactor =
+                                  Reactor
+                                      {
+                                        become =
+                                            \newBehavior ->
+                                                join $ up (Become newBehavior)
+
+                                      , die =
+                                            join $ up Die
+                                            
+                                      }
+
+                          in
+                              behavior_ signal (b reactor)
+                              
+
+                , merge =
+                      \signall signalr ->
+                          merge_ (\signal event -> join $ up (Trigger signal event)) signall signalr
+
+                , mapSignal =
+                      \f signal ->
+                          mapSignal_ (\signal event -> join $ up (Trigger signal event)) f signal
+
+                , filterSignal =
+                      \predicate initial ->
+                          filterSignal_ (\signal event -> join $ up (Trigger signal event)) predicate initial
+
+                }
+
+    in
+        linearize $
+            server +>> (knotted $ \up _ -> loop (ev up))
+        where
+
+            server initialRequest =
+                knotted $ \_ dn ->
+                    withRespond dn initialRequest
+                where
+
+                    withRespond respond =
+                        go
+                        where
+
+                            go req =
+                                case req of
+
+                                    Trigger signal event ->
+                                        undefined
