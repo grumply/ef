e3219c223030f48ff31ace2c64ab47253a3eec03
S. M. Hickman <sean@grump.ly>
2016-05-08 03:25:33 -0500

    Finish initial SVG implementation. This should complete v0.4.
    Reviewers: sean
    
    Reviewed By: sean
    
    Differential Revision: http://dev.ef-lang.org/D73

--- obvyus/closure.sh
+++ obvyus/closure.sh
@@ -1,1 +1,1 @@
-cp ./.stack-work/dist/x86_64-osx/Cabal-1.22.4.0_ghcjs/build/obvyus/obvyus.jsexe/all.js ~/Sites/main.js
+cp ./.stack-work/dist/x86_64-osx/Cabal-1.22.8.0_ghcjs/build/obvyus/obvyus.jsexe/all.js ~/Sites/main.js
