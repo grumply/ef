dac6c0351a3f8ed593aa48ef94594bae379321f5
S. M. Hickman <sean@grump.ly>
2016-11-06 11:02:04 -0600

    Reimplement Queue in terms of TMVar to avoid read/write contention.

--- ef-base/src/Data/Queue.hs
+++ ef-base/src/Data/Queue.hs
@@ -1,45 +1,38 @@
-{-# language ViewPatterns #-}
 module Data.Queue
   ( Queue
   , newQueue
+  , newQueueFrom
   , arrive
-  , arriveMany
   , collect
   ) where
 
+-- If anything more than this is needed, switch to TQueue.
+
 import Ef
 
-import Control.Concurrent
+import Control.Concurrent.STM
+import Data.Monoid
 
-import Data.Foldable as F
-
-data Queue a = Queue (MVar ()) (MVar [a])
+data Queue a = Queue {-# UNPACK #-} !(TMVar [a])
+  deriving Eq
 
 newQueue :: (Monad super, MonadIO super)
          => super (Queue a)
-newQueue = liftIO $ Queue <$> newMVar () <*> newEmptyMVar
+newQueue = liftIO $ Queue <$> newEmptyTMVarIO
+
+newQueueFrom :: (Monad super, MonadIO super)
+             => [a] -> super (Queue a)
+newQueueFrom q = liftIO $ Queue <$> newTMVarIO q
 
 arrive :: (Monad super, MonadIO super)
        => Queue a -> a -> super ()
-arrive (Queue barrier q) a = liftIO $
-  withMVar barrier $ \_ -> do
-    mq_ <- tryTakeMVar q
-    case mq_ of
-        Just q_ -> putMVar q (a:q_)
-        Nothing -> putMVar q [a]
-
-arriveMany :: (Monad super, MonadIO super, Foldable f)
-           => Queue a -> f a -> super ()
-arriveMany (Queue barrier q) (F.toList -> as) = liftIO $
-  withMVar barrier $ \_ -> do
-    mq_ <- tryTakeMVar q
-    case mq_ of
-        Just q_ -> putMVar q ((reverse as) ++ q_)
-        Nothing -> putMVar q (reverse as)
+arrive (Queue queue) a = liftIO $ atomically $ do
+  mq <- tryTakeTMVar queue
+  case mq of
+    Nothing -> putTMVar queue [a]
+    Just q  -> putTMVar queue (a:q)
 
 collect :: (Monad super, MonadIO super)
         => Queue a -> super [a]
-collect (Queue barrier q) = liftIO $
-  withMVar barrier $ \_ -> do
-    q_ <- takeMVar q
-    return $ reverse q_
+collect (Queue queue) = liftIO $
+  reverse <$> atomically (takeTMVar queue)
