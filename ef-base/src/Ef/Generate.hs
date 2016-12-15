fe7e33a0292478421bef2f5080453ca212a42530
sean <sean@grump.ly>
2016-12-15 10:36:23 -0600

    Upgrade Generate.

--- ef-base/src/Ef/Generate.hs
+++ ef-base/src/Ef/Generate.hs
@@ -1,104 +1,82 @@
-module Ef.Generate
-  ( Generator(..)
-  , generate
-  , each
-  , discard
-  , every
-  ) where
+module Ef.Generate (Generator(..), generate, each, discard, every) where
 
-import Ef.Narrative
+import Ef
 import Ef.Sync
-
-import Control.Applicative
-import Control.Monad
 import qualified Data.Foldable as F
-
 import Unsafe.Coerce
 
-newtype Generator self super a =
-    Select
-        { enumerate
-              :: ('[Sync] <: self, Monad super)
-              => Producer a self super ()
-        }
+newtype Generator ms c a = Select
+    { enumerate :: ('[Sync] <: ms, Monad c) => Producer a ms c ()
+    }
 
-instance Functor (Generator self super)
-  where
+instance Functor (Generator ms c) where
+    fmap f (Select p) = Select (p //> (producer . flip id . f))
 
-    fmap f (Select p) =
-        Select (p //> (producer . flip id . f))
-
-instance Applicative (Generator self super)
-  where
-
+instance Applicative (Generator ms c) where
     pure a = Select (producer ($ a))
-
     mf <*> mx =
         let produce f x = producer ($ f x)
-        in Select
-              $ for (enumerate mf)
-              $ for (enumerate mx)
-              . produce
+        in Select $ for (enumerate mf) $ for (enumerate mx) . produce
 
-instance Monad (Generator self super)
-  where
-
+instance (Functor (Messages ms), Monad c) => Monad (Generator ms c) where
     return a =
         let yields yield = yield a
         in Select (producer yields)
-
     m >>= f = Select $ for (enumerate m) (enumerate . f)
-
     fail _ = mzero
 
-instance Alternative (Generator self super)
-  where
-
+instance (Functor (Messages ms), Monad c) => Alternative (Generator ms c) where
     empty =
         let ignore = const (return ())
         in Select (producer ignore)
-
     p1 <|> p2 =
-        Select $ synchronized $ \up dn ->
-            let run xs = runSynchronized (enumerate xs) (unsafeCoerce up) (unsafeCoerce dn)
-            in do run p1
-                  run p2
+        Select $
+        synchronized $
+        \up dn ->
+             let run xs =
+                     runSynchronized
+                         (enumerate xs)
+                         (unsafeCoerce up)
+                         (unsafeCoerce dn)
+             in do run p1
+                   run p2
 
-instance MonadPlus (Generator self super)
-  where
-
+instance (Functor (Messages ms), Monad c) => MonadPlus (Generator ms c) where
     mzero = empty
-
     mplus = (<|>)
 
-instance Monoid (Generator self super a)
-  where
-
+instance (Functor (Messages ms), Monad c) => Monoid (Generator ms c a) where
     mempty = empty
-
     mappend = (<|>)
 
-generate :: ('[Sync] <: self, Monad super)
-         => Generator self super a -> Narrative self super ()
+generate
+    :: ('[Sync] <: ms, Monad c)
+    => Generator ms c a -> Code ms c ()
 generate l = runSync (enumerate (l >> mzero))
 
-each :: ('[Sync] <: self, Monad super, F.Foldable f)
-     => f a -> Producer' a self super ()
+each
+    :: ('[Sync] <: ms, Monad c, F.Foldable f)
+    => f a -> Producer' a ms c ()
 each xs =
     let yields yield = F.foldr (const . yield) (return ()) xs
     in producer yields
 
-discard :: Monad super => t -> Synchronized self a' a b' b super ()
+discard
+    :: (Functor (Messages ms), Monad c)
+    => t -> Synchronized a' a b' b ms c ()
 discard _ =
     let ignore _ _ = return ()
     in Synchronized ignore
 
-every :: ('[Sync] <: self, Monad super)
-      => Generator self super a -> Producer' a self super ()
-every it =
-    discard >\\ enumerate it
+every
+    :: ('[Sync] <: ms, Monad c)
+    => Generator ms c a -> Producer' a ms c ()
+every it = discard >\\ enumerate it
 
 {-# INLINE generate #-}
+
 {-# INLINE each #-}
+
 {-# INLINE discard #-}
+
 {-# INLINE every #-}
