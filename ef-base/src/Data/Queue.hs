c1d83a65b9d4f22dd9a0249f2bb1c10014c9de4e
S. M. Hickman <sean@grump.ly>
2017-01-18 07:03:13 -0600

    Switch to Chan to avoid dependency on STM when using GHCJS.

--- ef-base/src/Data/Queue.hs
+++ ef-base/src/Data/Queue.hs
@@ -11,14 +11,14 @@ import Ef
 #ifndef __GHCJS__
 import Control.Concurrent.Chan.Unagi
 #else
-import Control.Concurrent.STM
+import Control.Concurrent.Chan
 #endif
 import Data.Monoid
 
 #ifndef __GHCJS__
 data Queue a = Queue {-# UNPACK #-} !(InChan a) {-# UNPACK #-} !(OutChan a)
 #else
-data Queue a = Queue {-# UNPACK #-} !(TQueue a)
+data Queue a = Queue {-# UNPACK #-} !(Chan a)
 #endif
   deriving Eq
 
@@ -29,7 +29,7 @@ newQueue = do
   (i,o) <- liftIO newChan
   return (Queue i o)
 #else
-newQueue = liftIO $ Queue <$> newTQueueIO
+newQueue = liftIO $ Queue <$> newChan
 #endif
 
 arrive :: (MonadIO super)
@@ -37,12 +37,12 @@ arrive :: (MonadIO super)
 #ifndef __GHCJS__
 arrive (Queue i o) a = liftIO $ writeChan i a
 #else
-arrive (Queue ch) a = liftIO $ atomically $ writeTQueue ch a
+arrive (Queue ch) a = liftIO $ writeChan ch a
 #endif
 
 collect :: (MonadIO super) => Queue a -> super a
 #ifndef __GHCJS__
 collect (Queue i o) = liftIO $ readChan o
 #else
-collect (Queue ch) = liftIO $ atomically $ readTQueue ch
+collect (Queue ch) = liftIO $ readChan ch
 #endif
