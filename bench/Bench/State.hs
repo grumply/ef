d13d00b9dc14b42641131178dbf70f4365cc0398
grumply <sean@grump.ly>
2017-08-28 03:36:35 -0500

    Refine state benchmarks.

--- bench/Bench/State.hs
+++ bench/Bench/State.hs
@@ -20,44 +20,51 @@ suite = scope "state" $ tests
 state :: Test Sync ()
 state = tests
   [ addReturn
-  -- , fmapChain
+  , fmapChain
   ]
 
 addReturn :: Test Sync ()
 addReturn = scope "add/return" $ do
+  let (_,a) = runIdentity (interp ef_add_return 1)
+      b = runIdentity (T.evalStateT mtl_add_return 1)
+  expect (a == b)
   br1 <- nf "transformers" (runIdentity . T.evalStateT mtl_add_return) 1
   br2 <- nf "ef" (runIdentity . interp ef_add_return) 1
   report br1 br2
 
 fmapChain :: Test Sync ()
 fmapChain = scope "fmap/fmap/fmap" $ do
-  br1 <- nf "transformers" (runIdentity . T.evalStateT (fmap (+1) . fmap (+1) . fmap (+1) $ return (3 :: Int))) (1 :: Int)
-  br2 <- nf "ef" (runIdentity . interp (fmap (+1) . fmap (+1) . fmap (+1) $ return (3 :: Int))) (1 :: Int)
+  br1 <- nf "transformers" (runIdentity . T.evalStateT mtl_fmap) (1 :: Int)
+  br2 <- nf "ef" (runIdentity . interp ef_fmap) (1 :: Int)
   report br1 br2
+
+{-# INLINE mtl_fmap #-}
+mtl_fmap :: T.StateT Int Identity Int
+mtl_fmap = fmap (+1) . fmap (+1) . fmap (+1) $ T.get
+
+{-# INLINE ef_fmap #-}
+ef_fmap :: StateT Int Identity Int
+ef_fmap = fmap (+1) . fmap (+1) . fmap (+1) $ get
 
 {-# INLINE ef_add_return #-}
 ef_add_return :: StateT Int Identity Int
-ef_add_return = go (10000 :: Int)
-  where
-    {-# INLINE go #-}
-    go 0 = get
-    go n = do
-      x :: Int <- get
-      y :: Int <- get
-      put $! x + y
-      go (n - 1)
+ef_add_return = do
+  w :: Int <- get
+  x :: Int <- get
+  y :: Int <- get
+  z :: Int <- get
+  put $! w + x + y + z
+  get
 
 {-# INLINE mtl_add_return #-}
 mtl_add_return :: T.StateT Int Identity Int
-mtl_add_return = go (10000 :: Int)
-  where
-    {-# INLINE go #-}
-    go 0 = T.get
-    go n = do
-      x :: Int <- T.get
-      y :: Int <- T.get
-      T.put $! x + y
-      go (n - 1)
+mtl_add_return = do
+  w :: Int <- T.get
+  x :: Int <- T.get
+  y :: Int <- T.get
+  z :: Int <- T.get
+  T.put $! w + x + y + z
+  T.get
 
 type StateT s c a = Interp s (State s) c a
 
@@ -67,7 +74,7 @@ newtype State s k = State { runState :: s -> (s,k) }
 {-# INLINE eval #-}
 eval :: Monad c => Narrative (State s) c a -> s -> c (s,a)
 eval n = \s ->
-  Ef.thread (\(State ssk) s -> let (s',k) = ssk s in k s') n s
+  Ef.thread (\(State ssk) s -> let ~(s',k) = ssk s in k s') n s
 
 {-# INLINE interp #-}
 interp :: Monad c => StateT s c a -> s -> c (s,a)
